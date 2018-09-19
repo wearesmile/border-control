b6a1ccb1f46ed913e14b2b7a3839a153e4086213
James Glendenning <jamesglendenning1@gmail.com>
2018-09-19 09:34:16 +0100

    removed deaths

--- admin/class-border-control-admin.php
+++ admin/class-border-control-admin.php
@@ -527,8 +527,6 @@ class Border_Control_Admin {
 		if ( empty( $postarr ) )
 			return $data;
 
-			// echo '<pre>';var_dump($postarr);die;
-
 		if ( $this->sbc_is_controlled_cpt() ) : //$this->sbc_can_user_moderate()
 			$pending_review_email = false;
 			$blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
@@ -566,7 +564,6 @@ class Border_Control_Admin {
 					endforeach;
 
 				elseif ( isset( $postarr['publish'] ) || 'publish' === $postarr['post_status'] ) :
-					// echo'<pre>';var_dump( $postarr );die;
 					if ( 'pending' === $postarr['original_post_status'] || 'sbc_pending' === $postarr['original_post_status'] && $this->sbc_can_user_moderate() ) :
 						$owners = get_post_meta( $post_id, 'owners_owner', false );
 
