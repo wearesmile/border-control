8ef8a10036e0cd8e0e8ce085cbcd827a54badbfc
James Glendenning <jamesglendenning1@gmail.com>
2018-09-19 10:10:04 +0100

    version bump

--- border-control.php
+++ border-control.php
@@ -22,7 +22,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       border-control
  * Domain Path:       /languages
- * version:			  1.0.3
+ * version:			  1.0.4
  */
 
 // If this file is called directly, abort.
