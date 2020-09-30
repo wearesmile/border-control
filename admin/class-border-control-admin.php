32b1300fe06bf57ba9a4d67602a5764d7dda3f1b
Matt Oakley <matt.oakley@wearesmile.com>
2020-09-30 14:12:06 +0100

    Add setting that allows editors to publish in border control

--- admin/class-border-control-admin.php
+++ admin/class-border-control-admin.php
@@ -166,6 +166,30 @@ class Border_Control_Admin {
 			'sbc_borderControlPage_section'
 		);
 
+		add_settings_field(
+			'sbc_editors_can_publish',
+			__( 'Editors Can Publish', 'smile' ),
+			array( $this, 'sbc_editors_can_publish' ),
+			'borderControlPage',
+			'sbc_borderControlPage_section'
+		);
+
+	}
+
+	/**
+	 * Option box for enabling editor publishing
+	 */
+	public function sbc_editors_can_publish() {
+		$options = get_option( 'sbc_settings' );
+		?>
+			<?php
+			$name = 'sbc_can_publush';
+			?>
+			<input type="checkbox" name="sbc_settings[<?php esc_attr_e( $name ); ?>]" value="sbc_can_publush" <?php
+				if ( isset( $options[ $name ] ) && 'sbc_can_publush' === $options[ $name ] ) :
+				   echo 'checked="checked"';
+				endif; ?>>
+		<?php
 	}
 
 	/**
@@ -1150,7 +1174,8 @@ class Border_Control_Admin {
 		if ( ! is_admin() ) return;
 		$editor = get_role( 'editor' );
 
-		$editors_can_publish = apply_filters( 'sbc_can_editors_publish', false );
+		$options = get_option( 'sbc_settings' );
+		$editors_can_publish = $options['sbc_send_invite_email'] ?? false;
 
 		if ( $editor && false === $editors_can_publish ) {
 			// A list of capabilities to remove from editors.
